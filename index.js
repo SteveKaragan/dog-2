4f5da5e6e9420b6d57f3249069c39e8fe8b68a93
Stephen N. Karagan <stevekaragan@gmail.com>
2020-11-23 14:39:18 -0500

    final changes

--- index.js
+++ index.js
@@ -4,18 +4,29 @@
 function getDogImages(num) {
   fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => displayResults(responseJson));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  let images = responseJson.message.map(pic => {
+    return `<img src="${pic}" class="">`
+  })
+  $('.results-imgs').replaceWith(
+    `<div class="results-imgs">${images.join(" ")}</div>`
+  )
+  $('.results').removeClass('hidden');
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    let num = ($('input').val());
+    let num = $('input').val();
     getDogImages(num);
   });
 };
 
-$(function() {
+$(function () {
   console.log('App loaded! Waiting for submit!');
   watchForm();
 });
